fa34440a4cf78d6ed2fd3eb467d88ee12758d76c
Raphael Saldanha <rfsaldanha@gmail.com>
2024-03-14 18:52:24 +0100

    Change from document check to selection check

---
+++ R/check_selection.R
@@ -0,0 +1,110 @@
+#' Check current selection
+#'
+#'
+#' @param ask_modify logical
+#' @param language en-US, FR, etc
+#'
+#' @return nothing.
+#' @export
+check_selection <- function(ask_modify = TRUE, language = "auto"){
+  # Get active document id
+  doc_id <- rstudioapi::documentId(allowConsole = FALSE)
+
+  # Get selected content
+  selection <- rstudioapi::selectionGet(id = doc_id)
+
+  # Spell check the selected content
+  proof <- spell_check(text = selection$value, language = language)
+
+  # If proof is valid
+  adj <- 0
+  if(length(proof) > 0){
+    # For each response on proof...
+    for(i in 1:nrow(proof)){
+      # Original sentence
+      # Replace \r with \n for the console output
+      sentence <- gsub("\r", "\n", proof$context[[i]]$text)
+
+      # Print message about error found
+      error_message <- proof[[i, "message"]]
+      cli::cli_h1(error_message)
+
+      # Print the mistake
+      mistake <- substring(sentence,
+                           proof$context[[i]]$offset + 1,
+                           proof$context[[i]]$offset + proof$context[[i]]$length)
+
+      sentence_start <- substring(sentence,
+                                  0,
+                                  proof$context[[i]]$offset) |>
+        stringr::str_replace_all(".*\\n", "") # remove everything before and including \n
+
+      sentence_end <-  substring(sentence,
+                                 proof$context[[i]]$offset + proof$context[[i]]$length + 1,
+                                 nchar(sentence)) |>
+        stringr::str_replace_all("\\n.*$", "") # remove everything after and including \n
+
+      cli::cli_alert_danger(paste0(sentence_start, cli::col_red(mistake), sentence_end))
+
+      # If correction exists...
+      if (!is.na(proof$replacements[[i]][[1]]$value |> unlist())) {
+        # Print correction
+        correct_spelling <- proof$replacements[[i]][[1]]$value |> unlist()
+        cli::cli_alert_success(paste0(sentence_start, cli::col_green(correct_spelling), sentence_end))
+
+        # If user wants to modify the text
+        if(ask_modify){
+          # Ask if user agrees with the proposed modification
+          res_mod <- utils::menu(
+            choices = c("Yes", "No"),
+            title = "Do you agree with this modification?"
+          )
+
+          # If yes
+          if(res_mod == 1){
+
+            # List correction to be made,
+            # start and end position of the correction,
+            # and number of characters that were added or removed
+            # after correction
+            proof_tmp <- list(
+              correct_spelling = correct_spelling,
+              pos1 = proof[[i,"offset"]] + 1,
+              pos2 = proof[[i,"offset"]] + 1 + proof[[i,"length"]],
+              adj = nchar(correct_spelling) - nchar(mistake)
+            )
+
+            # For the first correction, use the original sentence
+            if(i == 1){
+              new_sentence <- paste0(
+                substring(selection$value, 0, proof_tmp$pos1-1),
+                proof_tmp$correct_spelling,
+                substring(selection$value, proof_tmp$pos2, nchar(selection$value))
+              )
+
+              adj = adj + proof_tmp$adj
+            } else {
+              # For the other corrections, use the last altered sentence and adjuste the position
+              new_sentence <- paste0(
+                substring(new_sentence, 0 + adj, proof_tmp$pos1-1 + adj),
+                proof_tmp$correct_spelling,
+                substring(new_sentence, proof_tmp$pos2 + adj, nchar(selection$value))
+              )
+
+              adj = adj + proof_tmp$adj
+            }
+
+            # Change the document content
+            rstudioapi::selectionSet(value = new_sentence, id = doc_id)
+
+          }
+
+        }
+      }
+    }
+  } else {
+    # Print message about no errors found
+    cli::cli_alert_success("No errors found!")
+  }
+}
+
--- R/spell_check.R
+++ R/spell_check.R
@@ -1,4 +1,31 @@
-spell_check <- function(text, language = "auto", ...){
+spell_check <- function(text, language = "auto"){
+  # Code adapted from https://github.com/nicucalcea/ggspell
+
+  # Call API
+  res <- httr2::request(base_url = "https://api.languagetool.org/v2/check") |>
+    httr2::req_body_form(
+      `text` = text,
+      `language` = language,
+      `enabledOnly` = "false"
+    ) |>
+    httr2::req_cache(tempdir()) |>
+    httr2::req_retry() |>
+    httr2::req_perform() |>
+    httr2::resp_body_json()
+
+  # Turn into a tibble
+  res <- res$matches |>
+    tibble::tibble() |>
+    tidyr::unnest_wider(1)
+
+  # Return tibble
+  return(res)
+}
+
+
+
+
+spell_check_old <- function(text, language = "auto", ...){
   # Code adapted from https://github.com/nicucalcea/ggspell
 
   # Prepare data for the API call
