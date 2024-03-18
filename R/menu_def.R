e08189871b0c5cea1b80537fdab2c2acb01397e7
Raphael Saldanha <rfsaldanha@gmail.com>
2024-03-18 13:56:31 +0100

    Menu prompt with default value.

---
+++ R/menu_def.R
@@ -0,0 +1,28 @@
+menu_def <- function(prompt, choices = c("Yes", "No"), def_pos = 1) {
+
+  choices <- sapply(choices, cli::format_inline, USE.NAMES = FALSE)
+
+  choices <- paste0(seq_along(choices), ": ", choices)
+
+  choices[def_pos] <- paste(choices[def_pos], "(Default)")
+
+  cli::cli_inform(
+    c(prompt, choices)
+  )
+
+  repeat {
+    selected <- readline("Selection: ")
+    if (selected %in% c("0", seq_along(choices))) {
+      break
+    }
+    if (selected == "") {
+      selected <- def_pos
+      break
+    }
+    cli::cli_inform("Enter an item from the menu, or 0 to exit")
+  }
+
+  selected <- as.integer(selected)
+
+  selected
+}
