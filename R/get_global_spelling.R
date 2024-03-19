c4f8bb68da81c73bdd49a00216d2e123e36941d2
Raphael Saldanha <rfsaldanha@gmail.com>
2024-03-19 10:57:04 +0100

    Simplify get_global_spelling function, with less dependencies.

--- R/get_global_spelling.R
+++ R/get_global_spelling.R
@@ -1,20 +1,6 @@
 get_global_spelling <- function(){
   # Read RStudio global settings
-
-  if (xfun::is_windows()) {
-    base <- rappdirs::user_config_dir("RStudio", appauthor = NULL)
-  } else {
-    base <- rappdirs::user_config_dir("rstudio", os = "unix")
-  }
-
-  res <- jsonlite::read_json(path = fs::path(base, "/rstudio-prefs.json"))
-
-  # Isolate spelling dictionary
-  if(is.null(res$spelling_dictionary_language)){
-    lang <- "en_US"
-  } else {
-    lang <- res$spelling_dictionary_language
-  }
+  lang <- rstudioapi::readRStudioPreference(name = "spelling_dictionary_language", default = "en_US")
 
   # Formatting
   lang <- stringr::str_replace(
