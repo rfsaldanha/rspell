320f19c214e7ec2576adf9fa981c6d3be8ed69f7
Raphael Saldanha <rfsaldanha@gmail.com>
2024-03-25 09:46:25 +0100

    Creates a function to get supported languages at the API.

---
+++ R/get_languages.R
@@ -0,0 +1,25 @@
+#' Get supported languages from LanguageTool API
+#'
+#' Returns a tibble of supported languages from LanguageTools API, with language name, code and long code.
+#'
+#' @return a tibble.
+#' @export
+#'
+#' @examplesIf curl::has_internet()
+#' get_languages()
+get_languages <- function(){
+  # Check if internet connection is available
+  if(!curl::has_internet()){
+    cli::cli_abort(message = "An Internet connection is needed to connect to the LanguageTools API. Please check your Internet connection.")
+  }
+
+  res <- httr2::request(base_url = "https://api.languagetool.org/v2/languages") |>
+    httr2::req_throttle(rate = 20/60) |>
+    httr2::req_retry() |>
+    httr2::req_perform() |>
+    httr2::resp_body_json() |>
+    tibble::tibble() |>
+    tidyr::unnest_wider(1)
+
+  return(res)
+}
